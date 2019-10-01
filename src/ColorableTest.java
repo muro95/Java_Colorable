89caaf2c390d7259e304909ccaa220a08b63b027
muro95 <sonbena95@gmail.com>
2019-10-01 09:10:43 +0700

    first commit

---
+++ src/ColorableTest.java
@@ -0,0 +1,16 @@
+public class ColorableTest {
+    public  static void main(String[] args){
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5.5);
+        shapes[1] = new Rectangle(5, 10);
+        shapes[2] = new Square(10);
+
+        for (Shape shape : shapes){
+            System.out.println(shape);
+            if (shape instanceof Square){
+                Colorable colorable = new Square();
+                System.out.println(colorable.howToColor());
+            }
+        }
+    }
+}
